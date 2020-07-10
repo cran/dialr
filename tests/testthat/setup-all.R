bfc276c239b17e27fd9e7591186ccbf2b7a8c55b
Danny Smith <danny@gorcha.org>
2020-07-10 05:50:03 +0000

    version 0.3.2

--- tests/testthat/setup-all.R
+++ tests/testthat/setup-all.R
@@ -5,17 +5,17 @@ ph_raw_types <- c(NA, "UNKNOWN", "MOBILE", "MOBILE", "FIXED_LINE", "FIXED_LINE_O
 
 ph_raw_timezone <-
   c(NA,
-    "Australia/Adelaide;Australia/Eucla;Australia/Lord_Howe;Australia/Perth;Australia/Sydney;Indian/Christmas;Indian/Cocos",
-    "Australia/Adelaide;Australia/Eucla;Australia/Lord_Howe;Australia/Perth;Australia/Sydney;Indian/Christmas;Indian/Cocos",
-    "Australia/Adelaide;Australia/Eucla;Australia/Lord_Howe;Australia/Perth;Australia/Sydney;Indian/Christmas;Indian/Cocos",
+    "Australia/Sydney",
+    "Australia/Sydney",
+    "Australia/Sydney",
     "Australia/Sydney",
     "America/New_York")
 
 ph_raw_timezone_strict <-
   c(NA,
     NA,
-    "Australia/Adelaide;Australia/Eucla;Australia/Lord_Howe;Australia/Perth;Australia/Sydney;Indian/Christmas;Indian/Cocos",
-    "Australia/Adelaide;Australia/Eucla;Australia/Lord_Howe;Australia/Perth;Australia/Sydney;Indian/Christmas;Indian/Cocos",
+    "Australia/Sydney",
+    "Australia/Sydney",
     "Australia/Sydney",
     "America/New_York")
 
@@ -25,8 +25,3 @@ regions <- regions[regions != "MF"]
   
 types <- get_types_for_region(regions)
 names(types) <- regions
-
-# Pre R 3.2.0 there is an issue with equality checks for external pointers.
-skip_equal <- function() {
-  skip_if(getRversion() < "3.2.0", "skip expect_equal tests pre R 3.2.0 - avoid 'cannot unclass an external pointer' error.")
-}
